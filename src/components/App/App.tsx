d12ce554a26496a6c3bbf278f96bebd5ae040b53
Cweili <3weili@gmail.com>
2024-12-26 17:04:11 +0800

    feat: tauri-astro-solid-template

---
+++ src/components/App/App.tsx
@@ -0,0 +1,51 @@
+import { createSignal } from 'solid-js';
+import { invoke } from '@tauri-apps/api/core';
+import logo from '../../assets/logo.svg?url';
+
+function App() {
+  const [greetMsg, setGreetMsg] = createSignal('');
+  const [name, setName] = createSignal('');
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name: name() }));
+  }
+
+  return (
+    <main class="container">
+      <h1>Welcome to Tauri + Astro + Solid</h1>
+
+      <div class="row">
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://astro.build" target="_blank">
+          <img src="/astro-dark.svg" class="logo astro dark-mode-only" alt="Astro logo" />
+          <img src="/astro-light.svg" class="logo astro light-mode-only" alt="Astro logo" />
+        </a>
+        <a href="https://solidjs.com" target="_blank">
+          <img src={logo} class="logo solid" alt="Solid logo" />
+        </a>
+      </div>
+      <p>Click on the Tauri, Astro, and Solid logos to learn more.</p>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg()}</p>
+    </main>
+  );
+}
+
+export default App;
